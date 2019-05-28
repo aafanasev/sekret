93ae6066c64442c87d33d788c514890f4500a4bb
aafanasev <tafanasyev@gmail.com>
2019-05-28 18:08:00 +0700

    add kotlin plugin dependencies

--- kotlin-plugin/build.gradle.kts
+++ kotlin-plugin/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     kotlin("jvm") version "1.3.21"
+    kotlin("kapt") version "1.3.21"
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("compiler-embeddable"))
+    
+    compileOnly("com.google.auto.service:auto-service:1.0-rc4")
+    kapt("com.google.auto.service:auto-service:1.0-rc4")
 }
