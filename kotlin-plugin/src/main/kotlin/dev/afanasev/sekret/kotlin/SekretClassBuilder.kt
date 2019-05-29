5b0667b4375d24a0b366872c00cefc7506ea1c09
aafanasev <tafanasyev@gmail.com>
2019-05-29 16:56:36 +0700

    add annotation check on fields

--- kotlin-plugin/src/main/kotlin/dev/afanasev/sekret/kotlin/SekretClassBuilder.kt
+++ kotlin-plugin/src/main/kotlin/dev/afanasev/sekret/kotlin/SekretClassBuilder.kt
@@ -2,14 +2,37 @@ package dev.afanasev.sekret.kotlin
 
 import org.jetbrains.kotlin.codegen.ClassBuilder
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilder
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
+import org.jetbrains.org.objectweb.asm.FieldVisitor
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 import org.jetbrains.org.objectweb.asm.Opcodes
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
 
 class SekretClassBuilder(private val classBuilder: ClassBuilder) : DelegatingClassBuilder() {
 
+    private val builtInAnnotation = FqName("dev.afanasev.sekret.Secret")
+    private val secretFields = mutableSetOf<String>()
+
     override fun getDelegate(): ClassBuilder = classBuilder
+
+    override fun newField(
+            origin: JvmDeclarationOrigin,
+            access: Int,
+            name: String,
+            desc: String,
+            signature: String?,
+            value: Any?
+    ): FieldVisitor {
+        (origin.descriptor as? PropertyDescriptor)?.backingField?.let {
+            if (it.annotations.hasAnnotation(builtInAnnotation)) {
+                secretFields.add(name)
+            }
+        }
+
+        return super.newField(origin, access, name, desc, signature, value)
+    }
 
     override fun newMethod(
             origin: JvmDeclarationOrigin,
@@ -28,7 +51,7 @@ class SekretClassBuilder(private val classBuilder: ClassBuilder) : DelegatingCla
         return object : MethodVisitor(Opcodes.ASM5, original) {
 
             override fun visitFieldInsn(opcode: Int, owner: String?, name: String?, descriptor: String?) {
-                if (opcode == Opcodes.GETFIELD && name == "password") {
+                if (opcode == Opcodes.GETFIELD && secretFields.contains(name)) {
                     InstructionAdapter(this).apply {
                         pop()
                         visitLdcInsn("■■■")
