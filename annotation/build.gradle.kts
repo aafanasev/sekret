4e601ad0ebadcce0c4147bb1bb9b776525d75686
aafanasev <tafanasyev@gmail.com>
2019-06-01 23:11:24 +0700

    add bintray uploading

--- annotation/build.gradle.kts
+++ annotation/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
     kotlin("jvm") version "1.3.21"
     `maven-publish`
@@ -6,4 +8,31 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+}
+
+
+publishing {
+    publications {
+        create("maven", MavenPublication::class.java) {
+            from(components["java"])
+            artifactId = "sekret-annotation"
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser") as? String ?: System.getenv("BINTRAY_USER")
+    key = project.findProperty("bintrayApiKey") as? String ?: System.getenv("BINTRAY_API_KEY")
+
+    setPublications("maven")
+
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = "sekret-annotation"
+        vcsUrl = "https://github.com/aafanasev/sekret.git"
+        publicDownloadNumbers = true
+
+        setLabels("kotlin", "data class", "toString")
+        setLicenses("Apache-2.0")
+    })
 }
--- kotlin-plugin/build.gradle.kts
+++ kotlin-plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
     kotlin("jvm") version "1.3.21"
     kotlin("kapt") version "1.3.21"
@@ -8,7 +10,33 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("compiler-embeddable"))
-    
+
     compileOnly("com.google.auto.service:auto-service:1.0-rc4")
     kapt("com.google.auto.service:auto-service:1.0-rc4")
+}
+
+publishing {
+    publications {
+        create("maven", MavenPublication::class.java) {
+            from(components["java"])
+            artifactId = "sekret-kotlin-plugin"
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser") as? String ?: System.getenv("BINTRAY_USER")
+    key = project.findProperty("bintrayApiKey") as? String ?: System.getenv("BINTRAY_API_KEY")
+
+    setPublications("maven")
+
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = "sekret-kotlin-plugin"
+        vcsUrl = "https://github.com/aafanasev/sekret.git"
+        publicDownloadNumbers = true
+
+        setLabels("kotlin", "data class", "toString")
+        setLicenses("Apache-2.0")
+    })
 }
