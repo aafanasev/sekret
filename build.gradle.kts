c5b28f30d3ba35cc456b67419c424e7779050636
aafanasev <tafanasyev@gmail.com>
2019-06-23 00:13:55 +0700

    update kotlin to 1.3.40

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.21" apply false
+    kotlin("jvm") version "1.3.40" apply false
 }
 
 allprojects {
