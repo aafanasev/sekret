af52ee5d2ecef65041c68a54ab78a9ddf227253c
aafanasev <tafanasyev@gmail.com>
2020-06-21 14:06:03 +0100

    update kotlin version to 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.40" apply false
+    kotlin("jvm") version "1.3.72" apply false
 }
 
 allprojects {
