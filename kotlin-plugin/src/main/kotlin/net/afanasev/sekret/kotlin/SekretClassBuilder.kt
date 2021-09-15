fbc56321af35388359292c8cad02b7fbdddef954
aafanasev <tafanasyev@gmail.com>
2021-09-16 00:56:30 +0100

    complete toString generation

--- kotlin-plugin/src/main/kotlin/net/afanasev/sekret/kotlin/SekretClassBuilder.kt
+++ kotlin-plugin/src/main/kotlin/net/afanasev/sekret/kotlin/SekretClassBuilder.kt
@@ -17,12 +17,11 @@ class SekretClassBuilder(
     internal val classBuilder: ClassBuilder,
     annotations: List<String>,
     private val mask: String,
-    private val maskNulls: Boolean
+    @Deprecated("to be removed") private val maskNulls: Boolean
 ) : DelegatingClassBuilder() {
 
     private val annotations: List<FqName> = annotations.map { FqName(it) }
-    private val secretFields = mutableSetOf<String>()
-    private val secretArrayFields = mutableSetOf<String>()
+    private val fields = linkedMapOf<String, Pair<String, Boolean>>()
 
     private var order = ""
     private var generateToString = false
@@ -38,14 +37,9 @@ class SekretClassBuilder(
         value: Any?
     ): FieldVisitor {
         if (generateToString) {
-            // TODO: reverse the logic
             (origin.descriptor as? PropertyDescriptor)?.let { descriptor ->
-                if (annotations.any { descriptor.annotations.hasAnnotation(it) }) {
-                    secretFields.add(name)
-                    if (desc.first() == '[') {
-                        secretArrayFields.add(name)
-                    }
-                }
+                val hide = annotations.any { descriptor.annotations.hasAnnotation(it) }
+                fields[name] = Pair(desc, hide)
             }
         }
         return super.newField(origin, access, name, desc, signature, value)
@@ -59,7 +53,7 @@ class SekretClassBuilder(
         signature: String?,
         exceptions: Array<out String>?
     ): MethodVisitor {
-        return if (name == "toString" && origin.descriptor?.isInDataClass() == true) {
+        return if (name == "toString" && isInDataClass(origin.descriptor)) {
             generateToString = true
             // skipping toString generation as it will be constructed manually in done() function.
             object : MethodVisitor(Opcodes.ASM5) {}
@@ -81,29 +75,82 @@ class SekretClassBuilder(
             JvmDeclarationOrigin.NO_ORIGIN, Opcodes.ACC_PUBLIC, "toString", "()Ljava/lang/String;", null, null,
         )
         mv.visitCode()
-//        mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder")
-//        mv.visitInsn(Opcodes.DUP)
-//        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false)
-        mv.visitLdcInsn("hello")
-//        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false)
-//        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false)
+        mv.visitTypeInsn(Opcodes.NEW, STRING_BUILDER)
+        mv.visitInsn(Opcodes.DUP)
+        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, STRING_BUILDER, "<init>", "()V", false)
+        mv.visitLdcInsn("${thisName.split("/").last()}(")
+        appendToStringBuilder(mv)
+
+        fields.onEachIndexed { index, entry ->
+            val name = entry.key
+            val desc = entry.value.first
+            val needToHide = entry.value.second
+
+            mv.visitLdcInsn((if (index == 0) "" else ", ") + "$name=")
+            appendToStringBuilder(mv)
+
+            if (needToHide) {
+                mv.visitLdcInsn(mask)
+                appendToStringBuilder(mv)
+            } else {
+                mv.visitVarInsn(Opcodes.ALOAD, 0)
+                mv.visitFieldInsn(Opcodes.GETFIELD, thisName, name, desc)
+
+                when {
+                    // primitives or descriptors supported by StringBuilder
+                    desc.length == 1 || STRING_BUILDER_SUPPORTED_DESCRIPTORS.contains(desc) -> {
+                        appendToStringBuilder(mv, desc)
+                    }
+                    // arrays
+                    desc[0] == '[' -> {
+                        val isPrimitiveArray = desc.length == 2
+                        mv.visitMethodInsn(
+                            Opcodes.INVOKESTATIC,
+                            "java/util/Arrays",
+                            "toString",
+                            "(${if (isPrimitiveArray) desc else "[Ljava/lang/Object;"})Ljava/lang/String;",
+                            false,
+                        )
+                        appendToStringBuilder(mv)
+                    }
+                    // others goes as Object
+                    else -> {
+                        appendToStringBuilder(mv, "Ljava/lang/Object;")
+                    }
+                }
+            }
+        }
+
+        mv.visitLdcInsn(")")
+        appendToStringBuilder(mv)
+
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, STRING_BUILDER, "toString", "()Ljava/lang/String;", false)
         mv.visitInsn(Opcodes.ARETURN)
-        //mv.visitMaxs(1, 1)
         mv.visitEnd()
+    }
+
+    private fun appendToStringBuilder(methodVisitor: MethodVisitor, descriptor: String = "Ljava/lang/String;") {
+        methodVisitor.visitMethodInsn(
+            Opcodes.INVOKEVIRTUAL,
+            STRING_BUILDER,
+            "append",
+            "($descriptor)Ljava/lang/StringBuilder;",
+            false,
+        )
     }
 
     private companion object {
         const val STRING_BUILDER = "java/lang/StringBuilder"
-        const val APPEND_METHOD = "append"
-        const val APPEND_DESCRIPTOR = "(Ljava/lang/String;)Ljava/lang/StringBuilder;"
+        val STRING_BUILDER_SUPPORTED_DESCRIPTORS =
+            setOf("Ljava/lang/String;", "Ljava/lang/StringBuffer;", "Ljava/lang/CharSequence;")
     }
 
     private fun JvmDeclarationOrigin.classDescriptor() =
         descriptor?.containingDeclaration?.fqNameSafe?.asString()?.replace('.', '/')
 
-    private fun DeclarationDescriptor.isInDataClass(): Boolean {
-        if (this is IrBasedSimpleFunctionDescriptor) {
-            return owner.origin == IrDeclarationOrigin.GENERATED_DATA_CLASS_MEMBER
+    private fun isInDataClass(descriptor: DeclarationDescriptor?): Boolean {
+        if (descriptor is IrBasedSimpleFunctionDescriptor) {
+            return descriptor.owner.origin == IrDeclarationOrigin.GENERATED_DATA_CLASS_MEMBER
         }
         return false
     }
--- sample/src/main/kotlin/net/afanasev/sekret/sample/App.kt
+++ sample/src/main/kotlin/net/afanasev/sekret/sample/App.kt
@@ -1,21 +1,42 @@
 package net.afanasev.sekret.sample
 
 import net.afanasev.sekret.Secret
+import java.lang.StringBuilder
 
 data class User(
+    val id: Int,
+    val lon: Long,
+    @Secret val flo: Float,
+    val dou: Double?,
     val login: String,
-    @Secret val password: String,
-    @net.afanasev.sekret.Secret val student: Student
+    val an: Any,
+    val buffer1: StringBuffer,
+    @Secret val buffer2: StringBuilder,
+    val char: Char,
+    @Secret val charSeq: CharSequence,
+    val bool: Boolean,
+    val charArr: CharArray,
+    @Secret val password: String?,
+    @net.afanasev.sekret.Secret val student: Student,
+    val array: Array<String>,
+    val list: List<Any>,
+    @Secret val Map: Map<String, Int>,
 )
 
 class Student(
-        val login: String,
-        val password: String
+    val login: String,
+    val password: String
 )
 
 fun main() {
     val student = Student("John", "Snow")
 
-    println(User("John", "Snow", student))
+    println(
+        User(
+            42, 10L, 3.2f, 55.6, "John",
+            "anything", StringBuffer(), StringBuilder(), 's', "char seq", false, charArrayOf('a', 'b', 'c'),
+            null, student, arrayOf("str1", "str2"), listOf("zyx", "111"), mapOf("twenty" to 20),
+        )
+    )
 
 }
